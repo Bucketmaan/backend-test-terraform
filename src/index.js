88a9879fb2a0cf5c6c616b4d8ca892ca9948b037
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-12-05 01:38:21 +0100

    feat(src/index.js): add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 import {
     initDb,
     getItems,
@@ -13,6 +14,7 @@ import {
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Swagger setup
